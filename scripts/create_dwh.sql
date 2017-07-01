86aebd0f60cd5f6556ceec8eccfe7536fecffb3f
cbritos <cbritos@technisys.com>
2017-07-01 16:29:21 -0300

    se comienza con req2

---
+++ scripts/create_dwh.sql
@@ -0,0 +1,13 @@
+-- Database: dwh
+
+-- DROP DATABASE dwh;
+
+CREATE DATABASE dwh
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'es_UY.UTF-8'
+       LC_CTYPE = 'es_UY.UTF-8'
+       CONNECTION LIMIT = -1;
+
+
---
+++ scripts/init.sql
@@ -0,0 +1,5 @@
+
+\i create_dwh_DB.sql
+\i create_auxiliar_DB.sql
+\i dwh_tables.sql
+\i auxiliar_tables.sql
