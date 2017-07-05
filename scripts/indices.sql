f9228a46ce4de6b35cea77c34a00ba678e988692
anylou <analia.louro@gmail.com>
2017-07-04 23:48:16 -0300

    Add files via upload

---
+++ scripts/indices.sql
@@ -0,0 +1,21 @@
+﻿-- Index: public.anio_mes
+
+-- DROP INDEX public.anio_mes;
+
+CREATE INDEX anio_mes
+  ON public.tiempo
+  USING btree
+  (anio, nombre_mes COLLATE pg_catalog."default");
+
+  
+  -- Index: public.fecha
+
+-- DROP INDEX public.fecha;
+
+CREATE INDEX fecha
+  ON public.tiempo
+  USING btree
+  (fecha);
+
+  
+  
